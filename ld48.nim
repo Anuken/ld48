3dbab0c5529ed02d091c272752fd275a52dd0f8f
Anuken <arnukren@gmail.com>
2021-04-25 17:26:56 -0400

    eggs

--- ld48.nim
+++ ld48.nim
@@ -28,6 +28,8 @@ type
     x, y, w, h: float32
   Shoot = enum
     sbasic, striple, scircle
+  Timers = object
+    eggs0, boiled0, toast1, eggs1, boiled1, formation, formation2: float32
 
 registerComponents(defaultComponentOptions):
   type
@@ -61,8 +63,15 @@ registerComponents(defaultComponentOptions):
       reload: float32
       sprite: string
       drop: float32
-    Spiker = object
+      val: float32
     Egg = object
+    Boiled = object
+    Toast = object
+    Fried = object
+    Pickled = object
+    Eggnog = object
+    Omlette = object
+    OmletteBig = object
 
 defineEffects:
 
@@ -70,9 +79,13 @@ defineEffects:
     fillCircle(e.x, e.y, 4.px, z = layerBloom, color = %"ff55ff")
     fillCircle(e.x, e.y, 2.px, z = layerBloom, color = %"ffc0ff")
 
+  standardBullet:
+    fillCircle(e.x, e.y, 4.px, z = layerBloom, color = e.color)
+    fillCircle(e.x, e.y, 2.px, z = layerBloom, color = colorWhite)
+
   pdeath(lifetime = 0.5):
     particles(e.id, 10, e.x, e.y, 14.px * e.fin):
-      fillCircle(x, y, 7.px * e.fout, color = %"ffc0ff")
+      fillCircle(x, y, 7.px * e.fout, color = %"ffffff")
 
   phit(lifetime = 0.2):
     particles(e.id, 5, e.x, e.y, 14.px * e.fin):
@@ -96,6 +109,7 @@ defineEffects:
 
 var lastPos: Vec2
 var lastPlayer: EntityRef
+var timers: Timers
 
 #region utilities
 
@@ -103,9 +117,13 @@ proc inBounds(value: Vec2): bool = value.x >= 0 and value.x <= worldw and value.
 
 template restart(start: bool = false) =
   sysAll.clearAll()
+  fau.time = 0f
+  timers = Timers()
+
+  for value in timers.fields:
+    value = rand(0f..1f)
 
   discard newEntityWith(Player(xs: 1f, ys: 1f), Pos(y: 5, x: worldh/2), Input(), Solid(), Health(value: 3), Hit(s: 0.6))
-  discard newEntityWith(Egg(), Enemy(sprite: "egg"), Pos(y: 5, x: worldw - 1), Solid(), Health(value: 5), Hit(s: 0.8))
 
   if not start:
     effectFlash(0, 0, col = colorWhite, life = 2f)
@@ -113,15 +131,15 @@ template restart(start: bool = false) =
     when not defined(debug):
       effectFlash(0, 0, col = colorBlack, life = 3f)
 
-macro shoot(t: untyped, ent: EntityRef, xp, yp, rot: float32, speed = 0.1, damage = 1, life = 400f, size = 0.3f) =
+macro shoot(t: untyped, ent: EntityRef, xp, yp, rot: float32, speed = 0.08f, damage = 1, life = 400f, size = 0.3f, color = colorWhite) =
   let effectId = ident("effectId" & t.repr.capitalizeAscii)
   result = quote do:
     let vel = vec2l(`rot`, `speed`)
     #hitEffect: effectIdHit,
-    discard newEntityWith(Pos(x: `xp`, y: `yp`), Timed(lifetime: `life`), Effect(id: `effectId`, rotation: `rot`), Bullet(shooter: `ent`), Hit(s: `size`), Vel(x: vel.x, y: vel.y), Damage(amount: `damage`))
+    discard newEntityWith(Pos(x: `xp`, y: `yp`), Timed(lifetime: `life`), Effect(id: `effectId`, rotation: `rot`, color: `color`), Bullet(shooter: `ent`), Hit(s: `size`), Vel(x: vel.x, y: vel.y), Damage(amount: `damage`))
 
-template makeEnemy(t: untyped, ey: float32, health: int = 5, hsize = 0.8f, dropchance = 0.1f) =
-  discard newEntityWith(t(), Enemy(sprite: "egg", drop: dropchance), Pos(y: ey, x: worldw + 1), Solid(), Health(value: health), Hit(s: hsize))
+template makeEnemy(t: untyped, ex = worldw + 1f, ey = worldh / 2f, health: int = 5, hsize = 0.8f, dropchance = 0.1f, spr: string = "", v = 0f) =
+  discard newEntityWith(t(), Enemy(sprite: spr, drop: dropchance, val: v), Pos(y: ey, x: ex), Solid(), Health(value: health), Hit(s: hsize))
 
 template timer(time: untyped, delay: float32, body: untyped) =
   time += fau.delta
@@ -136,9 +154,98 @@ template timer(time: untyped, delay: float32, body: untyped) =
 makeTimedSystem()
 
 sys("spawner", [Main]):
+  vars:
+    time: float32
   start:
-    if chance(0.01):
-      makeEnemy(Egg, rand(0f..worldh.float32))
+    #the game is split into several phases; each one lasts a few seconds, with 6 total
+    var phase = (fau.time / 40).int
+
+    #TODO remove later
+    when defined(debug):
+      phase = 6
+
+    case phase:
+      of 0:
+        timer(timers.eggs0, 14f):
+          let vy = rand(6f..(worldh-6f))
+          circlev(7, 4f):
+            makeEnemy(Egg, ex = worldw + 5f + x, ey = y + vy, spr = "egg")
+        timer(timers.boiled1, 3f):
+          makeEnemy(Boiled, ex = worldw + 1f, ey = rand(1f..(worldh-1f)))
+      of 1:
+        timer(timers.eggs0, 1.8f):
+          makeEnemy(Fried, ex = worldw + 1f, ey = rand(1f..(worldh-8f)), health = 7, hsize = 1f)
+        timer(timers.boiled1, 8f):
+          for v in [worldh - 1f, worldh - 2f, 1f, 2f]:
+            makeEnemy(Boiled, ex = worldw + 1f, ey = v, health = 4)
+      of 2:
+        timer(timers.boiled1, 0.45f):
+          makeEnemy(Boiled, ex = worldw + 1f, ey = timers.formation + 1, health = 4)
+          timers.formation += 1f
+          if timers.formation >= worldh - 1f:
+            timers.formation = 0
+            timers.boiled1 = -7f
+
+        timer(timers.eggs0, 12f):
+          let max = 5
+          for i in 1..max:
+            makeEnemy(Egg, ex = worldw + 1f, ey = worldh / max * i.float32 - 2f, spr = "egg")
+      of 3:
+        timer(timers.eggs0, 20f):
+          makeEnemy(Toast, ex = worldw + 0.5f, ey = worldh / 2f, health = 25, hsize = 1.2f)
+
+        timer(timers.boiled1, 4f):
+          makeEnemy(Fried, ex = worldw + 1f, ey = worldh/2f, health = 6, hsize = 1f)
+          makeEnemy(Fried, ex = worldw + 1f, ey = 1f, health = 6, hsize = 1f)
+      of 4:
+        timer(timers.eggs0, 5f):
+          makeEnemy(Omlette, ex = worldw + 1f, ey = worldh - 1f, health = 8, hsize = 1.2f, v = 181f.rad)
+          makeEnemy(Omlette, ex = worldw + 1f, ey = 1f, health = 8, hsize = 1.2f, v = 179f.rad)
+
+        timer(timers.boiled1, 0.3f):
+          for i in signs():
+            makeEnemy(Boiled, ex = worldw + 1f, ey = worldh/2f + (worldh/2f - (timers.formation + 0.75f)) * i, health = 4)
+          timers.formation += 1f
+          if timers.formation >= 3f:
+            timers.formation = 0
+            timers.boiled1 = -2f
+
+        timer(timers.eggs1, 15f):
+          makeEnemy(Toast, ex = worldw + 0.5f, ey = worldh / 2f, health = 22, hsize = 1.2f)
+      of 5:
+        timer(timers.boiled1, 0.75f):
+          for i in signs():
+            makeEnemy(Omlette, ex = worldw + 1f, ey = worldh/2f + (worldh/2f - (timers.formation * 2f + 0.75f)) * i, v = 181f.rad + i * 12.rad)
+          timers.formation += 1f
+          if timers.formation >= 4f:
+            timers.formation = 0
+            timers.boiled1 = -3f
+
+        timer(timers.boiled0, 0.75f):
+          for i in signs():
+            makeEnemy(Omlette, ex = worldw + 1f, ey = worldh/2f + ((timers.formation2 * 2f + 0.75f)) * i, v = 181f.rad + i * 14.rad)
+          timers.formation2 += 1f
+          if timers.formation2 >= 4f:
+            timers.formation2 = 0
+            timers.boiled0 = -3f
+
+        timer(timers.eggs1, 8f):
+          makeEnemy(Eggnog, ex = worldw + 1f, ey = worldh / 2f, health = 10, hsize = 1.1f, v = 180f.rad)
+      of 6:
+
+        timer(timers.eggs0, 5f):
+          makeEnemy(Eggnog, ex = worldw + 1f, ey = 0, health = 10, hsize = 1.1f, v = 140f.rad)
+          makeEnemy(Eggnog, ex = worldw + 1f, ey = worldh, health = 10, hsize = 1.1f, v = 220f.rad)
+      of 7:
+        timer(timers.eggs1, 7f):
+          makeEnemy(Pickled, ex = worldw + 1f, ey = worldh / 2f, health = 15, hsize = 1.3f)
+
+        timer(timers.boiled1, 2f):
+          makeEnemy(Fried, ex = worldw + 1f, ey = worldh/2f, health = 6, hsize = 1f)
+          makeEnemy(Fried, ex = worldw + 1f, ey = 1f, health = 6, hsize = 1f)
+      else:
+        #TODO
+        discard
 
 sys("controlled", [Input, Pos, Player]):
   all:
@@ -154,18 +261,21 @@ sys("controlled", [Input, Pos, Player]):
     if v.x.abs > 0:
       item.player.xs += sin(fau.time, 1f / 20f, 0.06)
 
+    #if v.y.abs > 0:
+    #  item.player.ys += 0.08f
+
     item.player.invuln -= fau.delta / invulnSecs
 
     case item.player.shoot:
       of sbasic:
         timer(item.player.reload, 0.1): shoot(frogb1, item.entity, item.pos.x, item.pos.y, 0, speed = 0.4f)
       of striple:
-        timer(item.player.reload, 0.1):
+        timer(item.player.reload, 0.15):
           shotgun(3, 10f):
             shoot(frogb1, item.entity, item.pos.x, item.pos.y, angle, speed = 0.4f)
       of scircle:
-        timer(item.player.reload, 0.12):
-          circle(6):
+        timer(item.player.reload, 0.13):
+          circle(9):
             shoot(frogb1, item.entity, item.pos.x, item.pos.y, angle, speed = 0.4f)
 
 sys("bullet", [Pos, Vel, Bullet, Hit]):
@@ -207,9 +317,8 @@ sys("collide", [Pos, Bullet, Hit]):
       if elem.entity != item.bullet.shooter and
         elem.entity != item.entity and
         elem.entity.alive and
-        item.bullet.shooter.alive and
         not elem.entity.has(Bullet) and
-        not(elem.entity.has(Enemy) and item.bullet.shooter.has(Enemy)) and
+        not(elem.entity.has(Enemy) and (not(item.bullet.shooter.alive) or item.bullet.shooter.has(Enemy))) and
         not(elem.entity.has(Player) and elem.entity.fetch(Player).invuln > 0):
 
         let
@@ -262,29 +371,85 @@ sys("powerup", [Powerup, Pos]):
     item.pos.x -= fau.delta * 0.7f
     if item.pos.x < -1f:
       item.entity.delete()
-    elif item.pos.vec2.within(lastPos, 0.6) and lastPlayer.alive:
+    elif item.pos.vec2.within(lastPos, 0.8) and lastPlayer.alive:
       effectPickup(item.pos.x, item.pos.y)
       lastPlayer.fetch(Player).shoot = item.powerup.shoot
       item.entity.delete()
 
-sys("spiker", [Spiker, Pos, Enemy]):
-  all:
-    timer(item.enemy.reload, 1):
-      circle(4):
-        shoot(shadowBullet, item.entity, item.pos.x, item.pos.y, angle + item.enemy.life / 2.0)
-
 sys("egg", [Egg, Pos, Enemy]):
   all:
-    item.pos.x -= 0.5f * fau.delta
-    timer(item.enemy.reload, 1):
-      circle(4):
-        shoot(shadowBullet, item.entity, item.pos.x, item.pos.y, angle + item.enemy.life / 2.0)
+    item.pos.x -= 1f * fau.delta
+    timer(item.enemy.reload, 1.1f):
+      circle(2):
+        shoot(standardBullet, item.entity, item.pos.x, item.pos.y, angle + item.enemy.life / 2.0, color = %"eec39a")
+
+sys("eggnog", [Eggnog, Pos, Enemy]):
+  all:
+    let m = vec2l(item.enemy.val, 7f * fau.delta)
+    item.pos.x += m.x
+    item.pos.y += m.y
+
+    timer(item.enemy.reload, 0.2f):
+      circle(2):
+        shoot(standardBullet, item.entity, item.pos.x, item.pos.y, angle + item.enemy.val + 90.rad, color = %"eec39a")
+
+sys("pickled", [Pickled, Pos, Enemy]):
+  all:
+    item.pos.x -= 4f * fau.delta
+
+    timer(item.enemy.reload, 0.1f):
+      item.enemy.val += 6f.rad
+      circle(2):
+        shoot(standardBullet, item.entity, item.pos.x, item.pos.y, angle + item.enemy.val + 90.rad + item.enemy.val, color = %"d8dff7")
+
+sys("boiled", [Enemy, Pos, Boiled]):
+  all:
+    if item.pos.x > worldw - 1f:
+      item.pos.x -= 0.85f * fau.delta
+    elif not item.enemy.val.awithin(90.rad, 0.01f):
+      item.enemy.val = aapproach(item.enemy.val, 90f.rad, 2f * fau.delta)
+    else:
+      item.pos.x -= 3.5f * fau.delta
+      timer(item.enemy.reload, 1.3f):
+        shoot(standardBullet, item.entity, item.pos.x, item.pos.y, 180f.rad, color = %"d8dff7", speed = 0.05f)
+
+sys("fried", [Enemy, Pos, Fried]):
+  all:
+    if item.pos.x > worldw - 1f:
+      item.pos.x -= 0.85f * fau.delta
+    elif not item.enemy.val.awithin(90.rad, 0.01f):
+      item.enemy.val = aapproach(item.enemy.val, 90f.rad, 2f * fau.delta)
+    else:
+      item.pos.x -= 3.5f * fau.delta
+      timer(item.enemy.reload, 1.3f):
+        shoot(standardBullet, item.entity, item.pos.x, item.pos.y, 180f.rad, color = %"d8dff7", speed = 0.05f)
+
+sys("omlette", [Enemy, Pos, Omlette]):
+  all:
+    let s = (item.enemy.val > 180.rad).sign
+    item.enemy.val += s * 0.14 * fau.delta
+
+    let m = vec2l(item.enemy.val, 6f * fau.delta)
+    item.pos.x += m.x
+    item.pos.y += m.y
+
+    timer(item.enemy.reload, 0.48f):
+      shoot(standardBullet, item.entity, item.pos.x, item.pos.y, item.enemy.val + 90.rad * s, color = %"e2cd4c")
+
+sys("toast", [Enemy, Pos, Toast]):
+  all:
+    item.pos.x -= 2f * fau.delta
+
+    timer(item.enemy.reload, 0.5f):
+      shotgun(3, 15):
+        shoot(standardBullet, item.entity, item.pos.x, item.pos.y, 180.rad + angle + sin(item.enemy.life, 4f.inv, 10f).rad, color = %"663931")
 
 sys("draw", [Main]):
   vars:
     buffer: Framebuffer
     bloom: Bloom
     bg: Texture
+
   init:
     sys.buffer = newFramebuffer()
     sys.bloom = newBloom()
@@ -320,10 +485,10 @@ sys("draw", [Main]):
     for i in 0..50:
       let
         sscl = r.rand(0.6..1.5)
-        cx = (r.rand(0..worldw) - fau.time * 15.0 * sscl + 10f).emod(worldw + 20f) - 10f
+        cx = (r.rand(0..worldw) - fau.time * 16.0 * sscl + 10f).emod(worldw + 20f) - 10f
         cy = r.rand(0..worldh)
         len = r.rand(3f..7f)
-      line(cx, cy, cx + len, cy)
+      line(cx, cy, cx + len, cy, color = alpha(0.8f))
 
     draw(1000, proc() =
       buf.pop()
@@ -367,16 +532,35 @@ sys("drawpowerup", [Pos, Powerup]):
       else: "powerup"
     draw(name.patch, item.pos.x, item.pos.y, mixcolor = rgba(1, 1, 1, absin(fau.time, 1f / 10f, 1f)))
 
-sys("drawSpiker", [Spiker, Pos, Enemy]):
-  all:
-    let s = 1f + sin(item.enemy.life, 0.1f, 0.1f)
-    draw("spiker-spikes".patch, item.pos.x, item.pos.y, rotation = item.enemy.life * 2.0, xscl = s, yscl = s)
-    draw("spiker".patch, item.pos.x, item.pos.y, xscl = s, yscl = s)
-
 sys("enemyDraw", [Enemy, Pos, Health]):
   all:
     if item.enemy.sprite != "":
       draw(item.enemy.sprite.patch, item.pos.x, item.pos.y, mixcolor = rgba(1, 1, 1, clamp(item.health.flash)))
+
+sys("drawboiled", [Boiled, Enemy, Pos, Health]):
+  all:
+    draw("boiled".patch, item.pos.x, item.pos.y, mixcolor = rgba(1, 1, 1, clamp(item.health.flash)), rotation = item.enemy.val)
+
+sys("drawpickled", [Pickled, Enemy, Pos, Health]):
+  all:
+    draw("pickled".patch, item.pos.x, item.pos.y, mixcolor = rgba(1, 1, 1, clamp(item.health.flash)), rotation = 90.rad)
+
+sys("drawfried", [Fried, Enemy, Pos, Health]):
+  all:
+    draw("fried".patch, item.pos.x, item.pos.y, mixcolor = rgba(1, 1, 1, clamp(item.health.flash)), rotation = item.enemy.val)
+
+sys("draweggnog", [Eggnog, Enemy, Pos, Health]):
+  all:
+    draw("eggnog".patch, item.pos.x, item.pos.y, mixcolor = rgba(1, 1, 1, clamp(item.health.flash)), rotation = item.enemy.val + 90.rad)
+
+sys("drawomlette", [Omlette, Enemy, Pos, Health]):
+  all:
+    draw("omlette".patch, item.pos.x, item.pos.y, mixcolor = rgba(1, 1, 1, clamp(item.health.flash)), rotation = item.enemy.val, yscl = (if item.enemy.val < 180f.rad: -1f else: 1f))
+
+sys("drawtoast", [Toast, Enemy, Pos, Health]):
+  all:
+    let s = sin(item.enemy.life, 1f / 10f, 0.2f)
+    draw("toast".patch, item.pos.x, item.pos.y, mixcolor = rgba(1, 1, 1, clamp(item.health.flash)), rotation = item.enemy.val, xscl = 1f + s, yscl = 1f - s)
 
 sys("all", [Pos]):
   init:
