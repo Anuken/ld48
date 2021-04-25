1b5699aad356349b98b0f920dc6d51790f41701e
Anuken <arnukren@gmail.com>
2021-04-25 12:28:39 -0400

    insanity

--- ld48.nim
+++ ld48.nim
@@ -1,4 +1,4 @@
-import ecs, presets/[basic, effects, content], math, sequtils, quadtree, simplex, random, bloom
+import ecs, presets/[basic, effects], math, sequtils, quadtree, random, bloom
 
 static: echo staticExec("faupack -p:assets-raw/sprites -o:assets/atlas")
 
@@ -6,8 +6,8 @@ static: echo staticExec("faupack -p:assets-raw/sprites -o:assets/atlas")
 
 const
   scl = 48f
-  worldSize = 20
-  gravity = 2f
+  worldh = 24
+  worldw = 30
   pspeed = 12f
   hitw = 0.6f
   hith = 1f - 2f/12f
@@ -22,19 +22,6 @@ const
   layerBloom = 50
 
 type
-  Rot = range[0..3]
-  Tile = object
-    back, wall: Block
-    rot: Rot
-
-  Block = ref object of Content
-    solid: bool
-    patches: seq[Patch]
-    top: Patch
-    border: bool
-    color: Color
-    sway: bool
-
   QuadRef = object
     entity: EntityRef
     x, y, w, h: float32
@@ -42,21 +29,17 @@ type
 registerComponents(defaultComponentOptions):
   type
     Vel = object
-      x, y, xdrag, ydrag: float32
-      onGround: bool
-      hang: float32
+      x, y: float32
 
     Hit = object
       s: float32
     Player = object
       xs, ys: float32
-      side: bool
-      wasGround: bool
+      shoot: float32
     Health = object
       value: int
 
     Input = object
-    Falling = object
     Solid = object
     Bullet = object
       shooter: EntityRef
@@ -69,15 +52,7 @@ registerComponents(defaultComponentOptions):
     Spiker = object
       reload: float32
 
-makeContent:
-  air = Block()
-  dirt = Block(solid: true, border: true, color: %"663931")
-  grass = Block(solid: false, sway: true)
-
 defineEffects:
-  jump(lifetime = 0.3):
-    particles(e.id, 5, e.x, e.y, 11.px * e.fin):
-      fillCircle(x, y, 7.px * e.fout, color = %"c3c3c3")
 
   shadowBullet:
     fillCircle(e.x, e.y, 4.px, z = layerBloom, color = %"ff55ff")
@@ -89,100 +64,33 @@ defineEffects:
 
   phit(lifetime = 0.2):
     particles(e.id, 5, e.x, e.y, 14.px * e.fin):
-      fillCircle(x, y, 3.px * e.fout, color = %"ffc0ff")
+      fillCircle(x, y, 3.px * e.fout, color = %"ffffff")
 
   bolt(lifetime = 0.1):
     fillPoly(e.x, e.y, 3, 8.px * e.fout, z = layerBloom, rotation = e.rotation, color = colorWhite)
 
-#endregion
+  frogb1:
+    fillCircle(e.x, e.y, 4.px, z = layerBloom, color = %"99e550")
+    fillCircle(e.x, e.y, 2.px, z = layerBloom, color = colorWhite)
 
-#region global variables
-
-var tiles = newSeq[Tile](worldSize * worldSize)
+  flash(lifetime = 1):
+    draw(fau.white, fau.cam.pos.x, fau.cam.pos.y, width = fau.cam.w, height = fau.cam.h, color = rgba(e.color.r, e.color.g, e.color.b, e.fout))
 
 #endregion
 
 #region utilities
 
-proc tile(x, y: int): Tile {.inline.} =
-  if x >= worldSize or y >= worldSize or x < 0 or y < 0: Tile(back: blockAir, wall: blockAir) else: tiles[x + y*worldSize]
+proc inBounds(value: Vec2): bool = value.x >= 0 and value.x <= worldw and value.y >= 0 and value.y <= worldh
 
-proc setWall(x, y: int, wall: Block) = tiles[x + y*worldSize].wall = wall
-proc setBack(x, y: int, back: Block) = tiles[x + y*worldSize].back = back
-proc solid(x, y: int): bool = tile(x, y).wall.solid
-proc empty(t: Tile): bool {.inline.} = t.wall.id == 0
-
-iterator eachTile*(): tuple[x, y: int, tile: Tile] =
-  const pad = 2
-  let
-    xrange = (fau.cam.w / 2).ceil.int + pad
-    yrange = (fau.cam.h / 2).ceil.int + pad
-    camx = fau.cam.pos.x.ceil.int
-    camy = fau.cam.pos.y.ceil.int
-
-  for cx in -xrange..xrange:
-    for cy in -yrange..yrange:
-      let
-        wcx = camx + cx
-        wcy = camy + cy
-
-      yield (wcx, wcy, tile(wcx, wcy))
-
-iterator allTiles*(): tuple[x, y: int, tile: Tile] =
-  for x in 0..<worldSize:
-    for y in 0..<worldSize:
-      yield (x, y, tiles[x + y * worldSize])
-
-template genWorld(cx, cy: int, first = false) =
-  for tile in tiles.mitems:
-    tile.wall = blockAir
-    tile.back = blockAir
-
-  clearAll(sysEnemy)
-  clearAll(sysBullet)
-
-  simplexSeed(rand(100000))
-
-  for x, y, t in allTiles():
-    if fractal(x.float, y.float, 2, freq = 0.1) > 0.2:
-      setBack(x, y, blockDirt)
-
-  #create container
-  for i in 0..<worldSize:
-    setWall(i, worldSize - 1, blockDirt)
-    setWall(0, i, blockDirt)
-    setWall(worldSize - 1, i, blockDirt)
-    setWall(i, 0, blockDirt)
-
-  discard newEntityWith(Enemy(), Spiker(), Pos(y: 5, x: rand(0.5f..worldSize.float32 - 1)), Solid(), Vel(), Health(value: 1), Hit(s: 0.9))
-
-  #for x, y, t in allTiles():
-  #  if t.empty and fractal(x.float, y.float + 30, 2, freq = 0.1) > 0.3:
-  #    setWall(x, y, t.back)
-
-  #create gaps
-  var
-    left = cx == 1 or chance(0.3)
-    right = cx == -1 or chance(0.3) or (first and not left)
-    top = cy == -1
-    bot = (chance(0.1) or not((left and cx != 1) or (right and cx != -1))) and not first
-
-  for i in 0..<worldSize:
-    if abs(i + 0.5 - worldSize/2f) <= 5:
-      if top: setWall(i, worldSize - 1, blockAir)
-      if left: setWall(0, i, blockAir)
-      if right: setWall(worldSize - 1, i, blockAir)
-      if bot: setWall(i, 0, blockAir)
-
-  #scatter grass
-  for x, y, t in allTiles():
-    if t.empty and tile(x, y - 1).wall == blockDirt and chance(0.5):
-      setWall(x, y, blockGrass)
-
-template restart =
+template restart(start: bool = false) =
   sysAll.clearAll()
-  genWorld(0, 1, true)
-  discard newEntityWith(Player(xs: 1f, ys: 1f), Pos(y: 5, x: worldSize/2), Input(), Falling(), Solid(), Vel(xdrag: 50, ydrag: 2), Health(value: 2), Hit(s: 0.8))
+  discard newEntityWith(Player(xs: 1f, ys: 1f), Pos(y: 5, x: worldh/2), Input(), Solid(), Health(value: 3), Hit(s: 0.8))
+
+  if not start:
+    effectFlash(0, 0, col = colorWhite, life = 2f)
+  else:
+    when not defined(debug):
+      effectFlash(0, 0, col = colorBlack, life = 3f)
 
 macro shoot(t: untyped, ent: EntityRef, xp, yp, rot: float32, speed = 0.1, damage = 1, life = 400f, size = 0.2f) =
   let effectId = ident("effectId" & t.repr.capitalizeAscii)
@@ -203,55 +111,24 @@ template timer(time: untyped, delay: float32, body: untyped) =
 
 makeTimedSystem()
 
-sys("controlled", [Input, Pos, Vel, Player]):
+sys("controlled", [Input, Pos, Player]):
   all:
-    let v = vec2(axis(keyA, keyD), 0).lim(1) * pspeed * fau.delta
-    item.vel.x += v.x
-    item.vel.y += v.y
-    if v.x < 0: item.player.side = true
-    if v.x > 0: item.player.side = false
+    let v = vec2(axis(keyA, keyD), axis(KeyCode.keyS, keyW)).lim(1) * pspeed * fau.delta
+    item.pos.x += v.x
+    item.pos.y += v.y
 
-    if item.vel.x.abs > 0:
+    if v.x.abs > 0:
       item.player.xs += sin(fau.time, 1f / 20f, 0.06)
 
-    if keySpace.tapped and item.vel.hang > 0:
-      item.vel.y += jumpvel
-      effectJump(item.pos.x, item.pos.y - hith/2f)
-      item.vel.hang = 0
-      item.player.ys = 2.1f
-      item.player.xs = 0.6f
-
-    #TODO attack
-    if keyMouseLeft.tapped and false:
-      let ang = (mouseWorld() - item.pos.vec2).nor * 0.5
-      item.vel.x += ang.x
-      item.vel.y += ang.y
-      shoot(bolt, item.entity, item.pos.x, item.pos.y, ang.angle, speed = 0.4f, life = 0.15f, size = 0.6f)
-      discard
-
-    template transition(cx, cy: int) =
-      item.pos.x = (item.pos.x + 0.5f).emod(worldSize) - 0.5f
-      item.pos.y = (item.pos.y + 0.5f).emod(worldSize) - 0.5f
-      item.pos.x = clamp(item.pos.x, -0.4, worldSize - 0.6)
-      item.pos.y = clamp(item.pos.y, -0.4, worldSize - 0.6)
-      genWorld(cx, cy)
-
-    #transition levels
-    if item.pos.x < -0.5: transition(-1, 0)
-    elif item.pos.y < -0.5: transition(0, -1)
-    elif item.pos.x > worldSize - 0.5: transition(1, 0)
-    #elif item.pos.x > worldSize: transition(1, 0)
-
-sys("falling", [Falling, Vel]):
-  all:
-    item.vel.y -= gravity * fau.delta
+    timer(item.player.shoot, 0.1):
+      shoot(frogb1, item.entity, item.pos.x, item.pos.y, 0, speed = 0.5f)
 
 sys("bullet", [Pos, Vel, Bullet, Hit]):
   all:
     item.pos.x += item.vel.x
     item.pos.y += item.vel.y
 
-    if collidesTiles(rectCenter(item.pos.x, item.pos.y, item.hit.s, item.hit.s), proc(x, y: int): bool = solid(x, y)):
+    if not inBounds(item.pos.vec2):
       effectPhit(item.pos.x, item.pos.y)
       item.entity.delete()
 
@@ -259,51 +136,27 @@ sys("bulletEffect", [Pos, Vel, Bullet, Effect]):
   all:
     item.effect.rotation = item.vel.vec2.angle
 
-sys("moveSolid", [Pos, Vel, Solid]):
+sys("vel", [Pos, Vel]):
   all:
-    let delta = moveDelta(rectCenter(item.pos.x, item.pos.y, hitw, hith), item.vel.x, item.vel.y, proc(x, y: int): bool = solid(x, y))
-    item.pos.x += delta.x
-    item.pos.y += delta.y
-    #is considered on ground when something is blocking the path down
-    item.vel.onGround = item.vel.y < 0 and delta.y.zero
-    if item.vel.onGround:
-      item.vel.hang = hangTime
-    else:
-      item.vel.hang -= fau.delta
-    if delta.x.zero: item.vel.x = 0
-    if delta.y.zero: item.vel.y = 0
+    item.pos.x += item.vel.x
+    item.pos.y += item.vel.y
 
-sys("momentum", [Vel]):
+sys("clampPos", [Pos]):
   all:
-    item.vel.x = clamp(item.vel.x, -maxvel, maxvel)
-    item.vel.y = clamp(item.vel.y, -maxvel, maxvel)
-    item.vel.x *= (1f - item.vel.xdrag * fau.delta)
-    item.vel.y *= (1f - item.vel.ydrag * fau.delta)
+    item.pos.x = clamp(item.pos.x, 0, worldw)
+    item.pos.y = clamp(item.pos.y, 0, worldh)
 
-sys("camfollow", [Input, Pos]):
-  all:
-    fau.cam.pos = vec2(worldSize.float32) / 2.0 - 0.5f
-    fau.cam.pos += vec2((fau.widthf mod scl) / scl, (fau.heightf mod scl) / scl) * fau.pixelScl
-    #fau.cam.pos = item.pos.vec2
-
-sys("playerground", [Player, Vel]):
-  all:
-    if not item.player.wasGround and item.vel.onGround:
-      item.player.xs = 1.8f
-      item.player.ys = 0.5f
-    item.player.wasGround = item.vel.onGround
-
-sys("quadtree", [Pos, Vel, Hit]):
+sys("quadtree", [Pos, Hit]):
   vars:
     tree: Quadtree[QuadRef]
   init:
-    sys.tree = newQuadtree[QuadRef](rect(-0.5, -0.5, worldSize + 1, worldSize + 1))
+    sys.tree = newQuadtree[QuadRef](rect(-0.5, -0.5, worldh + 1, worldh + 1))
   start:
     sys.tree.clear()
   all:
     sys.tree.insert(QuadRef(entity: item.entity, x: item.pos.x - item.hit.s/2.0, y: item.pos.y - item.hit.s/2.0, w: item.hit.s, h: item.hit.s))
 
-sys("collide", [Pos, Vel, Bullet, Hit]):
+sys("collide", [Pos, Bullet, Hit]):
   vars:
     output: seq[QuadRef]
   all:
@@ -353,96 +206,78 @@ sys("draw", [Main]):
   vars:
     buffer: Framebuffer
     bloom: Bloom
+    bg: Texture
   init:
     sys.buffer = newFramebuffer()
     sys.bloom = newBloom()
+    sys.bg = loadTextureStatic("space.png")
+    sys.bg.wrapRepeat()
 
     fau.pixelScl = 1f / tsize
-    initContent()
 
-    restart()
-
-    #load all block textures before rendering
-    for b in blockList:
-      b.top = (b.name & "-top").patch
-      var maxFound = 0
-      for i in 1..12:
-        if not fau.atlas.patches.hasKey(b.name & $i): break
-        maxFound = i
-
-      if maxFound == 0:
-        if fau.atlas.patches.hasKey(b.name):
-          b.patches = @[b.name.patch]
-      else:
-        b.patches = (1..maxFound).toSeq().mapIt((b.name & $it).patch)
+    restart(true)
 
   start:
     if keyEscape.tapped: quitApp()
 
     let
-      wsizepx = worldSize * tsize
+      wsizepx = worldh * tsize
       pixelation = (scl / tsize).int
-      aspect = fau.widthf / fau.heightf
 
-    fau.cam.resize(aspect * worldSize, worldSize)
+    fau.cam.resize(worldw, worldh)
+    fau.cam.pos = vec2(worldw / 2f, worldh / 2f)
     fau.cam.use()
 
-    sys.buffer.resize((worldSize * tsize * aspect).int, (worldSize * tsize).int)
+    sys.buffer.resize((worldw * tsize).int, (worldh * tsize).int)
     sys.buffer.push(colorClear)
     let buf = sys.buffer
     let bloom = sys.bloom
 
-    draw("bg".patch, worldSize/2f - 0.5f, worldSize/2f - 0.5f, z = -999)
+    var bgp = sys.bg.Patch
+    bgp.scroll(fau.time / 100f, 0f)
+    draw(bgp, worldw/2f, worldh/2f, z = -999)
+
+    var r = initRand(123)
+
+    for i in 0..50:
+      let
+        sscl = r.rand(0.6..1.5)
+        cx = (r.rand(0..worldw) - fau.time * 15.0 * sscl + 10f).emod(worldw + 20f) - 10f
+        cy = r.rand(0..worldh)
+        len = r.rand(3f..7f)
+      line(cx, cy, cx + len, cy)
 
     draw(1000, proc() =
       buf.pop()
-      buf.blitQuad()
+      screenMat()
+      draw(buf.texture, fau.widthf/2f, fau.heightf/2f, width = fau.heightf * worldw / worldh, height = -fau.heightf)
+      drawFlush()
       #replace with pause in the main bloom layer for smoother results
       #bloom.render()
     )
 
-    drawLayer(layerBloom, proc() = bloom.capture(), proc() = bloom.render())
-
-    var edge = "edge".patch
-
-    #draw all tiles
-    for x, y, t in eachTile():
-      let r = hashInt(x + y * worldSize)
-
-      #background
-      if t.back.id != 0:
-        draw(t.back.patches[r mod t.back.patches.len], x, y, z = layerBack, color = backCol)
-        for dx, dy, i in d4i():
-          if tile(x + dx, y + dy).back.id != t.back.id:
-            draw(edge, x, y, rotation = i * 90.rad, color = if i > 1: edgeLight * t.back.color * backCol else: edgeDark * t.back.color * backCol, z = layerBack + 0.1)
-
-      #draw wall stuff
-      if t.wall.id != 0:
-        let reg = t.wall.patches[r mod t.wall.patches.len]
-
-        if t.wall.sway:
-          let trns = noise((fau.time + x + y) / 2) * 6.px
-          drawv(reg, x, y, c2 = vec2(trns, 0), c3 = vec2(trns, 0))
-        else:
-          draw(reg, x, y)
-
-        #borders
-        if t.wall.border:
-          for dx, dy, i in d4i():
-            if tile(x + dx, y + dy).wall.id != t.wall.id:
-              draw(edge, x, y, rotation = i * 90.rad, color = if i > 1: edgeLight * t.wall.color else: edgeDark * t.wall.color)
-
-      #top region, if applicable
-      if t.wall.top.exists and not tile(x, y + 1).wall.solid:
-        draw(t.wall.top, x, y)
+    #drawLayer(layerBloom, proc() = bloom.capture(), proc() = bloom.render())
 
 
-sys("drawPlayer", [Player, Pos]):
+sys("drawPlayer", [Player, Pos, Health]):
   all:
     let alpha = 10.0 * fau.delta
     item.player.xs = item.player.xs.lerpc(1.0, alpha)
     item.player.ys = item.player.ys.lerpc(1.0, alpha)
-    draw("player".patch, item.pos.x, item.pos.y, xscl = item.player.xs * -item.player.side.sign, yscl = item.player.ys)
+    let
+      sscl = 10f.inv
+      ox = sin(fau.time, sscl, 0.1f)
+      oy = cos(fau.time, sscl, 0.1f)
+    draw("player".patch, item.pos.x + ox, item.pos.y + oy, xscl = item.player.xs, yscl = item.player.ys)
+    draw("boost".patch, item.pos.x - 4.px + ox, item.pos.y - 2.px + oy, xscl = 1f + sin(fau.time, 1f / 14f, 0.15f))
+
+    let max = item.health.value
+
+    draw(2001, proc() =
+      const size = 12f * 5f
+      for i in 0..<max:
+        draw("life".patch, 10f + i * (10f + size), fau.heightf - 10f, align = daTopLeft, width = size, height = size)
+    )
 
 sys("drawSpiker", [Spiker, Pos, Enemy]):
   all:
