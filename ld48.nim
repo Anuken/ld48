3ae77c4918998f8ae0d3a1ffb0cdb68f1188f05f
Anuken <arnukren@gmail.com>
2021-04-25 13:50:05 -0400

    powerups

--- ld48.nim
+++ ld48.nim
@@ -26,6 +26,8 @@ type
   QuadRef = object
     entity: EntityRef
     x, y, w, h: float32
+  Shoot = enum
+    sbasic, striple, scircle
 
 registerComponents(defaultComponentOptions):
   type
@@ -36,8 +38,9 @@ registerComponents(defaultComponentOptions):
       s: float32
     Player = object
       xs, ys: float32
-      shoot: float32
+      reload: float32
       invuln: float32
+      shoot: Shoot
     Health = object
       value: int
       flash: float32
@@ -49,11 +52,15 @@ registerComponents(defaultComponentOptions):
     Damage = object
       amount: int
 
+    Powerup = object
+      shoot: Shoot
+
     #enemies
     Enemy = object
       life: float32
       reload: float32
       sprite: string
+      drop: float32
     Spiker = object
     Egg = object
 
@@ -71,6 +78,10 @@ defineEffects:
     particles(e.id, 5, e.x, e.y, 14.px * e.fin):
       fillCircle(x, y, 3.px * e.fout, color = %"ffffff")
 
+  pickup(lifetime = 0.8):
+    particles(e.id, 6, e.x, e.y, 30.px * e.fin):
+      fillPoly(x, y, 3, 8.px * e.fout, angle(x, y), color = %"99e550")
+
   bolt(lifetime = 0.1):
     fillPoly(e.x, e.y, 3, 8.px * e.fout, z = layerBloom, rotation = e.rotation, color = colorWhite)
 
@@ -79,11 +90,12 @@ defineEffects:
     fillCircle(e.x, e.y, 2.px, z = layerBloom, color = colorWhite)
 
   flash(lifetime = 1):
-    draw(fau.white, fau.cam.pos.x, fau.cam.pos.y, width = fau.cam.w, height = fau.cam.h, color = rgba(e.color.r, e.color.g, e.color.b, e.fout))
+    draw(fau.white, fau.cam.pos.x, fau.cam.pos.y, width = fau.cam.w, height = fau.cam.h, color = rgba(e.color.r, e.color.g, e.color.b, e.fout * e.color.a))
 
 #endregion
 
 var lastPos: Vec2
+var lastPlayer: EntityRef
 
 #region utilities
 
@@ -108,8 +120,8 @@ macro shoot(t: untyped, ent: EntityRef, xp, yp, rot: float32, speed = 0.1, damag
     #hitEffect: effectIdHit,
     discard newEntityWith(Pos(x: `xp`, y: `yp`), Timed(lifetime: `life`), Effect(id: `effectId`, rotation: `rot`), Bullet(shooter: `ent`), Hit(s: `size`), Vel(x: vel.x, y: vel.y), Damage(amount: `damage`))
 
-template makeEnemy(t: untyped, ey: float32, health: int = 5, hsize = 0.8f) =
-  discard newEntityWith(t(), Enemy(sprite: "egg"), Pos(y: ey, x: worldw + 1), Solid(), Health(value: health), Hit(s: hsize))
+template makeEnemy(t: untyped, ey: float32, health: int = 5, hsize = 0.8f, dropchance = 0.1f) =
+  discard newEntityWith(t(), Enemy(sprite: "egg", drop: dropchance), Pos(y: ey, x: worldw + 1), Solid(), Health(value: health), Hit(s: hsize))
 
 template timer(time: untyped, delay: float32, body: untyped) =
   time += fau.delta
@@ -135,6 +147,7 @@ sys("controlled", [Input, Pos, Player]):
     item.pos.y += v.y
     item.pos.x = clamp(item.pos.x, 0, worldw)
     item.pos.y = clamp(item.pos.y, 0, worldh)
+    lastPlayer = item.entity
 
     lastPos = item.pos.vec2
 
@@ -143,8 +156,17 @@ sys("controlled", [Input, Pos, Player]):
 
     item.player.invuln -= fau.delta / invulnSecs
 
-    timer(item.player.shoot, 0.1):
-      shoot(frogb1, item.entity, item.pos.x, item.pos.y, 0, speed = 0.4f)
+    case item.player.shoot:
+      of sbasic:
+        timer(item.player.reload, 0.1): shoot(frogb1, item.entity, item.pos.x, item.pos.y, 0, speed = 0.4f)
+      of striple:
+        timer(item.player.reload, 0.1):
+          shotgun(3, 10f):
+            shoot(frogb1, item.entity, item.pos.x, item.pos.y, angle, speed = 0.4f)
+      of scircle:
+        timer(item.player.reload, 0.12):
+          circle(6):
+            shoot(frogb1, item.entity, item.pos.x, item.pos.y, angle, speed = 0.4f)
 
 sys("bullet", [Pos, Vel, Bullet, Hit]):
   all:
@@ -208,12 +230,17 @@ sys("collide", [Pos, Bullet, Hit]):
 
               whenComp(target, Player):
                 player.invuln = 1f
+                effectFlash(0, 0, col = %"ff464688", life = 0.3f)
 
         if kill:
           if target.has Player:
             restart()
             break
           else:
+            whenComp(target, Enemy):
+              if chance(enemy.drop):
+                whenComp(target, Pos):
+                  discard newEntityWith(Pos(x: pos.x, y: pos.y), Powerup(shoot: rand(striple..Shoot.high)))
             target.delete()
 
         effectPhit(item.pos.x, item.pos.y)
@@ -228,6 +255,16 @@ sys("enemy", [Enemy, Pos]):
   all:
     item.enemy.life += fau.delta
     if item.pos.x < -1f:
+      item.entity.delete()
+
+sys("powerup", [Powerup, Pos]):
+  all:
+    item.pos.x -= fau.delta * 0.7f
+    if item.pos.x < -1f:
+      item.entity.delete()
+    elif item.pos.vec2.within(lastPos, 0.6) and lastPlayer.alive:
+      effectPickup(item.pos.x, item.pos.y)
+      lastPlayer.fetch(Player).shoot = item.powerup.shoot
       item.entity.delete()
 
 sys("spiker", [Spiker, Pos, Enemy]):
@@ -292,6 +329,8 @@ sys("draw", [Main]):
       buf.pop()
       screenMat()
       draw(buf.texture, fau.widthf/2f, fau.heightf/2f, width = fau.heightf * worldw / worldh, height = -fau.heightf)
+      for i in signs():
+        draw("border".patch, fau.widthf/2f + i * fau.heightf * worldw / worldh / 2f, fau.heightf/2f, height = fau.heightf, width = 3f * 4f)
       drawFlush()
       #replace with pause in the main bloom layer for smoother results
       #bloom.render()
@@ -319,6 +358,14 @@ sys("drawPlayer", [Player, Pos, Health]):
       for i in 0..<max:
         draw("life".patch, 10f + i * (10f + size), fau.heightf - 10f, align = daTopLeft, width = size, height = size)
     )
+
+sys("drawpowerup", [Pos, Powerup]):
+  all:
+    let name = case item.powerup.shoot:
+      of striple: "p-triple"
+      of scircle: "p-circle"
+      else: "powerup"
+    draw(name.patch, item.pos.x, item.pos.y, mixcolor = rgba(1, 1, 1, absin(fau.time, 1f / 10f, 1f)))
 
 sys("drawSpiker", [Spiker, Pos, Enemy]):
   all:
